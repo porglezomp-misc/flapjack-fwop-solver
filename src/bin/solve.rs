7a70b37d0b38585d26c96135d0997be0f13a7cfd
Justin Hao <djhao@umich.edu>
2018-12-31 17:27:13 -0800

    :construction: Implemented new K/V cache write

--- src/bin/solve.rs
+++ src/bin/solve.rs
@@ -9,6 +9,31 @@ use fwop::{generate_map, output, parse};
 
 const MASK: u32 = (1 << 25) - 1;
 
+// fn generate_fwopcache<P: AsRef<Path>>(path: P) -> io::Result<File> {
+//     println!("No game cache found, generating...");
+//     let mut file = File::create(path)?;
+//     let mut cache = BTreeMap::<u32, (u32, u32)>::new();
+//     for i in 0..1 << 25 {
+//         let map = generate_map(i);
+//         cache
+//             .entry(map)
+//             .and_modify(|(x, count)| {
+//                 let new_count = i.count_ones();
+//                 if new_count < *count {
+//                     *x = i;
+//                     *count = new_count;
+//                 }
+//             })
+//             .or_insert_with(|| (i, i.count_ones()));
+//     }
+//     for i in 0..1 << 25 {
+//         let value = cache.get(&i).map(|&x| x.0).unwrap_or(0);
+//         file.write(&value.to_ne_bytes())?;
+//     }
+//     file.flush()?;
+//     Ok(file)
+// }
+
 fn generate_fwopcache<P: AsRef<Path>>(path: P) -> io::Result<File> {
     println!("No game cache found, generating...");
     let mut file = File::create(path)?;
@@ -26,13 +51,14 @@ fn generate_fwopcache<P: AsRef<Path>>(path: P) -> io::Result<File> {
             })
             .or_insert_with(|| (i, i.count_ones()));
     }
-    for i in 0..1 << 25 {
-        let value = cache.get(&i).map(|&x| x.0).unwrap_or(0);
-        file.write(&value.to_ne_bytes())?;
+    for (k, v) in cache.entries() {
+        file.write(k.to_ne_bytes())?;
+        file.write(&v.to_ne_bytes())?;
     }
     file.flush()?;
     Ok(file)
 }
+
 
 fn solve_from_cache(cache: &mut File, x: u32) -> io::Result<()> {
     cache.seek(io::SeekFrom::Start(x as u64 * 4))?;
