11c075c5d96aaae167d0275202a3d9c8289b21ac
Justin Hao <djhao@umich.edu>
2019-01-02 07:27:24 -0800

    :tada: Finished writing function to get entry from cache using K/V

--- src/bin/solve.rs
+++ src/bin/solve.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp::Ordering::{Equal, Greater, Less},
     collections::BTreeMap,
     fs::File,
     io::{self, Read, Seek, Write},
@@ -51,14 +52,68 @@ fn generate_fwopcache<P: AsRef<Path>>(path: P) -> io::Result<File> {
             })
             .or_insert_with(|| (i, i.count_ones()));
     }
-    for (k, v) in cache.entries() {
-        file.write(k.to_ne_bytes())?;
-        file.write(&v.to_ne_bytes())?;
+
+    for (k, v) in &cache {
+        file.write(&k.to_ne_bytes())?;
+        file.write(&v.0.to_ne_bytes())?;
     }
     file.flush()?;
     Ok(file)
 }
 
+// fn get_entry_from_cache<F: Read, F:Seek>(cache: F) -> Option<u64> {
+
+// }
+
+// fn write_cache<F: Write>(cache: &BTreeMap<u32, (u32, u32)>, buf: &mut F) {
+
+// }
+
+fn get_entry_from_cache(cache: &mut File, x: u32) -> Option<u32> {
+    let mut base = 0;
+    let mut size = 1 << 23; // Num entries in cache
+    while size > 0 {
+        size /= 2;
+        let mid = (base + size) / 2;
+        // get this entry from cache
+        let mut buf = [0; 4];
+        cache.seek(io::SeekFrom::Start(mid as u64 * 8)).unwrap();
+        cache
+            .read_exact(&mut buf)
+            .map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::UnexpectedEof,
+                    "Failed to find a solution in the cache",
+                )
+            })
+            .unwrap();
+
+        let key = u32::from_ne_bytes(buf);
+        cache
+            .seek(io::SeekFrom::Start((mid as u64 * 8) + 4))
+            .unwrap();
+        cache
+            .read_exact(&mut buf)
+            .map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::UnexpectedEof,
+                    "Failed to find a solution in the cache",
+                )
+            })
+            .unwrap();
+
+        let value = u32::from_ne_bytes(buf);
+
+        // Do the binary search comparision
+        base = match key.cmp(&x) {
+            Less => mid,
+            Greater => base,
+            Equal => return Some(value),
+        }
+    }
+    // check cache[start] and cache[end] fit the target and return those keys values
+    return None;
+}
 
 fn solve_from_cache(cache: &mut File, x: u32) -> io::Result<()> {
     cache.seek(io::SeekFrom::Start(x as u64 * 4))?;
